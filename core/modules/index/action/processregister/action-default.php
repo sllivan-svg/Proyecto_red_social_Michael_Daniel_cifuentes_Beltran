f0ddbe369eb74d3df8d3f443809a36a6ebb06208
Daniel B <62016160+sllivan-svg@users.noreply.github.com>
2020-05-16 09:22:17 -0500

    Red_Social_programacion_tercer_semestre

---
+++ core/modules/index/action/processregister/action-default.php
@@ -0,0 +1,59 @@
+<?php
+/**
+* @author evilnapsis
+* @brief Proceso de registo, obtiene los valores post del formulario de registro y los guarda en la bd.
+**/
+
+		if(!empty($_POST)){
+			if($_POST["name"]!=""&&$_POST["lastname"]!=""&&$_POST["email"]!=""&&$_POST["password"]!=""){
+				$user = UserData::getByEmail($_POST["email"]);
+				if($user==null){
+					$str = "abcdefghijklmopqrstuvwxyz1234567890";
+					$code = "";
+					for ($i=0; $i < 6; $i++) { 
+						$code .= $str[rand(0,strlen($str)-1)];
+					}
+					
+	
+					$user = new UserData();
+					$user->name = $_POST["name"];
+					$user->lastname = $_POST["lastname"];
+					$user->email = $_POST["email"];
+					$user->password = sha1(md5($_POST["password"]));
+					$user->code = $code;
+					$u = $user->add();
+
+					$p =  new ProfileData();
+					$p->user_id = $u[1];
+					$p->add();
+
+
+					$msg = "<body><h1>Registro Exitoso</h1></body>";
+	
+					mail($_POST["email"], "Registro Exitoso", $msg);
+					/* $f = fopen (ROOT."/register.txt","w");
+					fwrite($f, $msg);
+					fclose($f);
+					*/
+					Core::alert("Registro Exitoso!, se ha enviado un correo electronico con los datos necesarios para activar su cuenta.");
+					Core::redir("./");
+				}else{
+					Core::alert("El email proporcionado ya esta registrado.");
+					Core::redir("./");
+
+				}
+			}else{
+				Core::alert("No puede dejar campos vacios");				
+				Core::redir("./");
+			}
+		}
+
+//		Core::redir("./");
+		//View::render($this,"index",array("meta"=>$meta));
+	
+
+
+
+
+
+?>
